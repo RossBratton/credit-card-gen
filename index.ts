af0ba0dd95e57b15352bb31564ab9ca496b8862f
Ross Bratton <ross.bratton@rewardinsight.com>
2023-10-16 11:44:39 +0100

    Updated to allow for multiple files to be generated at once

--- index.ts
+++ index.ts
@@ -9,50 +9,60 @@ const fileTypes = ['card', 'customer', 'transaction', 'maintenance', 'eligibilit
 const fileType = process.argv[2];
 const rowCount = process.argv[3] ? parseInt(process.argv[3]) : 10000;
 
-if (fileTypes.indexOf(fileType) === -1) {
-    printProgress(`Invalid file type ${fileType}. Must be one of ${fileTypes.join(', ')}.`);
+if (fileTypes.indexOf(fileType) === -1 && fileType !== 'all') {
+    printProgress(`Invalid file type ${fileType}. Must be one of the following: all, ${fileTypes.join(', ')}.`);
     process.exit(1);
 }
 
-printProgress(`Generating ${rowCount} rows of data...`);
-
-const workbook = new Excel.Workbook();
-const worksheet = workbook.addWorksheet(`${fileType} Data`);
-const excelColumns: Partial<Column>[] = getColumns(fileType);
-
-worksheet.columns = excelColumns;
-
-// Build the data in chunks to avoid memory issues
-const data = [];
-const chunkSize = (rowCount < 10000) ? rowCount : 10000;
-const chunkCount = Math.ceil(rowCount / chunkSize);
-
-for (let i = 0; i < chunkCount; i++) {
-    printProgress("Generating chunk " + (i + 1) + " of " + chunkCount);
-    const chunk = generateData(fileType, chunkSize);
-    data.push(chunk);
-    printProgress("Finished chunk " + (i + 1) + " of " + chunkCount);
+if (isNaN(rowCount)) {
+    printProgress(`Invalid row count ${rowCount}. Must be a number.`);
+    process.exit(1);
 }
 
-data.forEach((chunk: any[], index: number) => {
-    printProgress("Writing chunk " + (index + 1) + " of " + chunkCount);
-    worksheet.addRows(chunk);
-    printProgress("Finished writing chunk " + (index + 1) + " of " + chunkCount);
-});
+const fileTypesToGenerate = (fileType === 'all') ? fileTypes : [fileType];
 
-const fileDirectory = path.resolve(__dirname, `outputs/${fileType}`);
 
-if (!fs.existsSync(fileDirectory)){
-    fs.mkdirSync(fileDirectory);
+for (let type of fileTypesToGenerate) {
+    printProgress(`Generating ${rowCount} rows of data...`);
+
+    const workbook = new Excel.Workbook();
+    const worksheet = workbook.addWorksheet(`${type} Data`);
+    const excelColumns: Partial<Column>[] = getColumns(type);
+
+    worksheet.columns = excelColumns;
+
+    // Build the data in chunks to avoid memory issues
+    const data = [];
+    const chunkSize = (rowCount < 10000) ? rowCount : 10000;
+    const chunkCount = Math.ceil(rowCount / chunkSize);
+
+    for (let i = 0; i < chunkCount; i++) {
+        printProgress("Generating chunk " + (i + 1) + " of " + chunkCount);
+        const chunk = generateData(type, chunkSize);
+        data.push(chunk);
+        printProgress("Finished chunk " + (i + 1) + " of " + chunkCount);
+    }
+
+    data.forEach((chunk: any[], index: number) => {
+        printProgress("Writing chunk " + (index + 1) + " of " + chunkCount);
+        worksheet.addRows(chunk);
+        printProgress("Finished writing chunk " + (index + 1) + " of " + chunkCount);
+    });
+
+    const fileDirectory = path.resolve(__dirname, `outputs/${type}`);
+
+    if (!fs.existsSync(fileDirectory)){
+        fs.mkdirSync(fileDirectory);
+    }
+
+    const exportPath = `${fileDirectory}/${type}_${rowCount}_rows_${new Date().toISOString()}.csv`;
+
+    printProgress(`Writing to ${exportPath}...`);
+        workbook.csv.writeFile(exportPath).then(() => {
+    }).catch((err) => {
+        console.error('Error writing file.');
+        console.error(err);
+    }).finally(() => {
+        printProgress('Finished.', true);
+    });   
 }
-
-const exportPath = `${fileDirectory}/${fileType}_${rowCount}_rows_${new Date().toISOString()}.csv`;
-
-printProgress(`Writing to ${exportPath}...`);
-    workbook.csv.writeFile(exportPath).then(() => {
-}).catch((err) => {
-    console.error('Error writing file.');
-    console.error(err);
-}).finally(() => {
-    printProgress('Finished.', true);
-});
